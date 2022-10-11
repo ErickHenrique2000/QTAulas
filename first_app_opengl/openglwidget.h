aafdf6179ed6b00ac1119e9777990a306126dee9
Erick Henrique Silva <erick.henrique@L40XB-a2952f.UFABC_DOMINIO>
2022-10-11 09:34:36 -0300

    a

--- first_app_opengl/openglwidget.h
+++ first_app_opengl/openglwidget.h
@@ -5,12 +5,35 @@
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions_4_0_Core>
 #include <QTimer>
+#include <QVector4D>
+#include <vector>
+#include <QFile>
 
 class OpenGLWidget : public QOpenGLWidget, public QOpenGLFunctions_4_0_Core
 {
     Q_OBJECT
 public:
     explicit OpenGLWidget(QWidget *parent = nullptr);
+    ~OpenGLWidget();
+
+    GLuint vboVertices{0};
+    GLuint vboColors{0};
+    GLuint eboIndices{0};
+    GLuint vao{0};
+
+    std::vector<QVector4D> vertices;
+    std::vector<QVector4D> colors;
+    std::vector<GLuint> indices;
+
+    GLuint shaderProgram{0};
+
+    void createVBOs();
+    void createShaders();
+
+    void destroyVBOs();
+    void destroyShaders();
+
+    void changeDiagonal();
 
 signals:
 
